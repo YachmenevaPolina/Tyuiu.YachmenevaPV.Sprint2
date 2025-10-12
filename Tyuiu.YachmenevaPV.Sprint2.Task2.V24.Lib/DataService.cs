7a907cf3fbd45cc2385979600fe74c34baf0b0eb
Tyuiu.YachmenevaPV@outlook.com <poliviya7@gmail.com>
2025-10-12 18:50:03 +0500

    Task2

--- Tyuiu.YachmenevaPV.Sprint2.Task2.V24.Lib/DataService.cs
+++ Tyuiu.YachmenevaPV.Sprint2.Task2.V24.Lib/DataService.cs
@@ -15,9 +15,8 @@ namespace Tyuiu.YachmenevaPV.Sprint2.Task2.V24.Lib
         (y == 8 && x >= 6 && x <= 10) ||
         (y == 9 && x >= 4 && x <= 12) ||
         (y == 10 && x >= 4 && x <= 12) ||
-        (y == 11 && x >= 2 && x <= 10)
-        )
-            {
+        (y == 11 && x >= 2 && x <= 10) )
+            { 
                 res = true;
             }
             else
