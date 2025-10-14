dec19574043d838d2eda9d942c3131b6b8f901b4
Tyuiu.YachmenevaPV@outlook.com <poliviya7@gmail.com>
2025-10-14 12:39:20 +0500

    Task3

---
+++ Tyuiu.YachmenevaPV.Sprint2.Task3.V2.Test/DataServiceTest.cs
@@ -0,0 +1,50 @@
+﻿using Tyuiu.YachmenevaPV.Sprint2.Task3.V2.Lib;
+namespace Tyuiu.YachmenevaPV.Sprint2.Task3.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 4;
+            double res = ds.Calculate(x);
+            double wait = - 44.654;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double res = ds.Calculate(x);
+            double wait = 1.5;
+            Assert.AreEqual(wait, res);
+
+        }
+
+        [TestMethod]
+        public void ValidCondition3()
+        {
+            DataService ds = new DataService();
+            double x = -2;
+            double res = ds.Calculate(x);
+            double wait = -53;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition4()
+        {
+            DataService ds = new DataService();
+            double x = -7;
+            double res = ds.Calculate(x);
+            double wait = -77;
+            Assert.AreEqual(wait, res);
+        }
+
+
+    }
+}
---
+++ Tyuiu.YachmenevaPV.Sprint2.Task3.V2/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.YachmenevaPV.Sprint2.Task3.V2.Lib;
+{
+    DataService ds = new DataService();
+    Console.Title = "Спринт #2 | Выполнила: Ячменёва П. В. | РППб-25-1";
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* Спринт #2                                                               *");
+    Console.WriteLine("* Тема: Вложенные операторы if - else.                                    *");
+    Console.WriteLine("* Задание #3                                                              *");
+    Console.WriteLine("* Вариант #2                                                             *");
+    Console.WriteLine("* Выполнила: Ячменёва Полина Викторовна | РППб-25-1                       *");
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* УСЛОВИЕ:                                                                *");
+    Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y с    *");
+    Console.WriteLine("* использованием вложенных операторов if-else, где пользователь вводит    *");
+    Console.WriteLine("* значение перменной Х с клавиатуры.                                      *");
+    Console.WriteLine("*                                                                         *");
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+    Console.WriteLine("***************************************************************************");
+
+    Console.WriteLine("Введите значение переменной Х: ");
+    double x = Convert.ToDouble(Console.ReadLine());
+    double res = ds.Calculate(x);
+
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+    Console.WriteLine("***************************************************************************");
+
+    Console.WriteLine("Значение функции = " + res);
+
+    Console.ReadKey();
+}
