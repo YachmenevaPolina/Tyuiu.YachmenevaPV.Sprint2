b73e5ab3159485dabd3a17bc2db7deceb71703e0
Tyuiu.YachmenevaPV@outlook.com <poliviya7@gmail.com>
2025-10-14 14:54:03 +0500

    Task5

---
+++ Tyuiu.YachmenevaPV.Sprint2.Task5.V4.Lib/DataService.cs
@@ -0,0 +1,29 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.YachmenevaPV.Sprint2.Task5.V4.Lib
+{
+    public class DataService : ISprint2Task5V4
+    {
+        public string FindCardSuit(int value)
+        {
+            string res;
+            switch(value)
+            {
+                case 1:
+                    res = "Пики";
+                    break;
+                case 2:
+                    res = "Трефы";
+                    break;
+                case 3:
+                    res = "Бубны";
+                    break;
+                case 4:
+                    res = "Червы";
+                    break;
+                default:
+                    throw new ArgumentException($"Масть должна быть от 1 до 4. Знчение {value}");
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.YachmenevaPV.Sprint2.Task5.V4/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.YachmenevaPV.Sprint2.Task5.V4.Lib;
+{
+    DataService ds = new DataService();
+    Console.Title = "Спринт #2 | Выполнила: Ячменёва П. В. | РППб-25-1";
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* Спринт #2                                                               *");
+    Console.WriteLine("* Тема: Оператор switch.                                                  *");
+    Console.WriteLine("* Задание #5                                                              *");
+    Console.WriteLine("* Вариант #4                                                              *");
+    Console.WriteLine("* Выполнила: Ячменёва Полина Викторовна | РППб-25-1                       *");
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* УСЛОВИЕ:                                                                *");
+    Console.WriteLine("* Написать программу, которая использует оператор switch, вычисляет       *");
+    Console.WriteLine("* требуемое значение и возвращает результат.                              *");
+    Console.WriteLine("*                                                                         *");
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+    Console.WriteLine("***************************************************************************");
+    
+    Console.WriteLine("Введите номер масти: ");
+    int numCard = Convert.ToInt32(Console.ReadLine());
+
+    string res;
+    if ((numCard < 1) || (numCard > 4))
+    {
+        res = "Введено неверное значение!";
+    }
+    else
+    {
+        res = "Это масть - " + ds.FindCardSuit(numCard);
+    }
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+    Console.WriteLine("***************************************************************************");
+
+    Console.WriteLine(res);
+
+    Console.ReadKey();
+}
