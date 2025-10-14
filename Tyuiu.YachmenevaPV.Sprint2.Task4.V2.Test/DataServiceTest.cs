f6dca34e1cb48bccd27945fd715b7f17fa5ffcb9
Tyuiu.YachmenevaPV@outlook.com <poliviya7@gmail.com>
2025-10-14 13:11:50 +0500

    Task4

---
+++ Tyuiu.YachmenevaPV.Sprint2.Task4.V2.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.YachmenevaPV.Sprint2.Task4.V2.Lib;
+namespace Tyuiu.YachmenevaPV.Sprint2.Task4.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 6;
+            double y = 2;
+            double res = ds.Calculate(x, y);
+            double wait = 9.5;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 6;
+            double y = 8;
+            double res = ds.Calculate(x, y);
+            double wait = 1.3;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.YachmenevaPV.Sprint2.Task4.V2/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.YachmenevaPV.Sprint2.Task4.V2.Lib;
+{
+    DataService ds = new DataService();
+    Console.Title = "Спринт #2 | Выполнила: Ячменёва П. В. | РППб-25-1";
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* Спринт #2                                                               *");
+    Console.WriteLine("* Тема: Тернарный оператор.                                               *");
+    Console.WriteLine("* Задание #4                                                              *");
+    Console.WriteLine("* Вариант #2                                                              *");
+    Console.WriteLine("* Выполнила: Ячменёва Полина Викторовна | РППб-25-1                       *");
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* УСЛОВИЕ:                                                                *");
+    Console.WriteLine("* Написать программу, которая вычисляет требуемое значение с              *");
+    Console.WriteLine("* использованием тернарного оператора, где пользователь вводит значение   *");
+    Console.WriteLine("* переменных х, у с клавиатуры, если х > y + 3.                           *");
+    Console.WriteLine("*                                                                         *");
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+    Console.WriteLine("***************************************************************************");
+
+    Console.WriteLine("Введите значение переменной Х: ");
+    double x = Convert.ToDouble(Console.ReadLine()); 
+
+    Console.WriteLine("Введите значение переменной Y: ");
+    double y = Convert.ToDouble(Console.ReadLine());
+    double res = ds.Calculate(x, y);
+
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+    Console.WriteLine("***************************************************************************");
+
+    Console.WriteLine("Значение функции = " + res);
+
+    Console.ReadKey();
+
+}
